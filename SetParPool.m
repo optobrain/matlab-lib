ceeffc01dc80579c1f3e5b65140a670ab7eb874e
optobrain <46489364+optobrain@users.noreply.github.com>
2019-01-28 13:57:11 -0500

    Update SetParPool.m

--- SetParPool.m
+++ SetParPool.m
@@ -1,11 +1,15 @@
-function SetParPool(nworker)
+function SetParPool(nworker,minIdle)
+
+if nargin < 2
+    minIdle = 30;
+end
 
 p = gcp('nocreate');
 if isempty(p)
-    parpool(nworker);
+    parpool(nworker,'IdleTimeout',minIdle);
 else
     if p.NumWorkers ~= nworker
         delete(p);
-        parpool(nworker);
+        parpool(nworker,'IdleTimeout',minIdle);
     end
 end
